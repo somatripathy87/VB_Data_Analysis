08beded96190dafc87343a58bde8191b9f4582b4
Soumyakant Tripathy <154498833+somatripathy87@users.noreply.github.com>
2024-01-07 21:21:27 -0700

    Commit_Jan_7th_2024
    Data analysis using VBA - refactored code

--- Stock.vb
+++ Stock.vb
@@ -1,13 +1,9 @@
-Public Sub StockTickerHeaders()
-    ' Adding headers
-    Range("I1:L1").Font.Bold = True
-    Range("I1").Value = "Ticker"
-    Range("J1").Value = "Yearly Change"
-    Range("K1").Value = "Percent Change"
-    Range("L1").Value = "Total Stock Volume"
-End Sub
+
 
 Sub CalculateStockSummary()
+   
+
+    'Defining all variables
     Dim ws As Worksheet
     Dim lastRow As Long
     Dim arrData As Variant
@@ -19,9 +15,8 @@ Sub CalculateStockSummary()
     Dim percentChange As Double
     Dim totalVolume As Double
     Dim outputRow As Long
-    Dim dateValue As Long
-
-    ' Variables to track the maximum and minimum values
+   
+    'Variables to track the maximum and minimum values
     Dim maxPercentIncrease As Double
     Dim maxPercentIncreaseTicker As String
     Dim maxPercentDecrease As Double
@@ -29,30 +24,39 @@ Sub CalculateStockSummary()
     Dim maxTotalVolume As Double
     Dim maxTotalVolumeTicker As String
 
-    ' Set reference to the active sheet
-    For Each ws In Worksheets
+    'Apply loop to run through each sheet,
+      For Each ws In Worksheets
+        
+    'Activate current sheet
         ws.Activate
+       
+    'Set reference to the active sheet
        Set ws = ActiveSheet
 
-        StockTickerHeaders
+     ' Adding headers from columns I through L
+    Range("I1:L1").Font.Bold = True
+    Range("I1").Value = "Ticker"
+    Range("J1").Value = "Yearly Change"
+    Range("K1").Value = "Percent Change"
+    Range("L1").Value = "Total Stock Volume"
 
-        ' Find the last row in column A
-        lastRow = ws.Cells(ws.Rows.Count, "A").End(xlUp).Row
+    'Find the last row in column A
+        lastRow = ws.Cells(ws.Rows.Count, 1).End(xlUp).Row
 
-          ' Read all data in the sheet into an array
+    'Read all data in the sheet into an array
         arrData = ws.Range("A2:G" & lastRow).Value
 
-        ' Initialize output row to 2 to skip the header
+    'Initialize output row to 2 to skip the header
         outputRow = 2
         
-        ' Initialize ticker value
+    'Initialize ticker value
         ticker = Range("A2").Value
         
-        ' Initialize open and close price
+    'Initialize open and close price
         openPrice = Range("C2").Value
         closePrice = Range("F2").Value
 
-        ' Initialize variables for maximum and minimum percent values and maximum volume
+    'Initialize variables for maximum and minimum percent values and maximum volume
         maxPercentIncrease = 0
         maxPercentIncreaseTicker = ""
         maxPercentDecrease = 0
@@ -61,19 +65,16 @@ Sub CalculateStockSummary()
         maxTotalVolumeTicker = ""
 
 
-
-        ' Loop through each row in the array
+    'Loop through each row in the array
         For i = 1 To UBound(arrData)
-            ' Skip if the row has been processed
+    ' Skip if the row has been processed
             If arrData(i, 1) = "" Then GoTo ContinueLoop
 
-            ' Get the values in the current row
+    ' Get the values in the current row
             ticker = arrData(i, 1)
-            dateValue = arrData(i, 2)
             openPrice = arrData(i, 3)
             closePrice = arrData(i, 6)
-            volume = arrData(i, 7)
-
+            
             ' Reset variables for each ticker
             
             percentChange = 0
@@ -93,16 +94,16 @@ Sub CalculateStockSummary()
 
             'Calculate yearly change by subtracting last close price of the ticker with the first open price
             yearlyChange = closePrice - openPrice
+            
             ' Calculate percent change and also look for avoiding division by zero
-            If openPrice <> 0 Then
-                percentChange = (yearlyChange / openPrice)
-            End If
+            percentChange = (yearlyChange / openPrice)
+            
 
             ' Write results to columns I through L
             ws.Cells(outputRow, 9).Value = ticker
             ws.Cells(outputRow, 10).Value = yearlyChange
             ws.Cells(outputRow, 11).Value = percentChange
-              ws.Cells(outputRow, 11).NumberFormat = "0.00%"
+            ws.Cells(outputRow, 11).NumberFormat = "0.00%" 'Change format to show percentage
             ws.Cells(outputRow, 12).Value = totalVolume
 
             ' Color formatting
@@ -134,10 +135,10 @@ Sub CalculateStockSummary()
 ContinueLoop:
         Next i
 
-        ' Output the greatest percentage increase, greatest percentage decrease, and greatest total volume
+        ' Print the greatest percentage increase, greatest percentage decrease, and greatest total volume
         ws.Range("P1").Value = "Ticker"
         ws.Range("Q1").Value = "Value"
-        Range("P1:Q1").Font.Bold = True
+        Range("P1:Q1").Font.Bold = True 'Change format of headers to show as Bold
         
         ws.Range("O2").Value = "Greatest % Increase"
         ws.Range("O3").Value = "Greatest % Decrease"
@@ -148,10 +149,15 @@ ContinueLoop:
         ws.Range("P4").Value = maxTotalVolumeTicker
         
         ws.Range("Q2").Value = maxPercentIncrease
+        ws.Range("Q2").NumberFormat = "0.00%" 'Change format to show percentage
         ws.Range("Q3").Value = maxPercentDecrease
+        ws.Range("Q3").NumberFormat = "0.00%" 'Change format to show percentage
         ws.Range("Q4").Value = maxTotalVolume
         
        Next ws
 End Sub
+
+
+
 
 
